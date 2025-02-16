c3e1ed014d1dd5e2dd361aba1a143624af762677
Huân Nè <112363749+Ken290906@users.noreply.github.com>
2025-02-16 17:18:27 +0700

    ...

---
+++ src/main/java/com/example/graduation_project_group_2_mobileworld/GraduationProjectGroup2MobileWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.graduation_project_group_2_mobileworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationProjectGroup2MobileWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraduationProjectGroup2MobileWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/graduation_project_group_2_mobileworld/GraduationProjectGroup2MobileWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graduation_project_group_2_mobileworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationProjectGroup2MobileWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
