b23adcbb7c36cdde8f872c78312291a6959c85d1
Huân Nè <112363749+Ken290906@users.noreply.github.com>
2025-02-17 12:59:56 +0700

    Test done !2

---
+++ front-end/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {ref, onMounted} from "vue";
+import axios from "axios";
+
+const message = ref("");
+
+onMounted(async () => {
+  const response = await  axios.get("http://localhost:8080/api/hello");
+  message.value = response.data;
+})
+
+</script>
+
+<template>
+  <h1>{{message}}</h1>
+</template>
+
