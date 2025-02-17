f4ddf31041a9e784c092c433719c5ed69733a107
lqphuc2872006 <lequangphuc2872006@gmail.com>
2025-02-17 13:33:09 +0700

    Thêm packge

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import {ref, onMounted} from "vue";
 import axios from "axios";
-
 const message = ref("");
 
 onMounted(async () => {
@@ -13,5 +12,8 @@ onMounted(async () => {
 
 <template>
   <h1>{{message}}</h1>
+  <br>
+  <h1>Ledear the`m bưởi</h1>
+  <img src="/picture/ledear.jpg" alt="">
 </template>
 
