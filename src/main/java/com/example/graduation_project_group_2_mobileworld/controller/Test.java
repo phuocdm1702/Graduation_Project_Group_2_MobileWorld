1e2083a82ae0a728e5ce8ffafdb756f2f98b4ef3
phuocdm1702 <githupstudy@gmail.com>
2025-02-26 11:10:32 +0700

    commit HoaDon

---
+++ src/main/java/com/example/graduation_project_group_2_mobileworld/controller/Test.java
@@ -0,0 +1,18 @@
+package com.example.graduation_project_group_2_mobileworld.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api")
+public class Test {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Bạn Huan mua da`u bưởi!";
+    }
+
+}
