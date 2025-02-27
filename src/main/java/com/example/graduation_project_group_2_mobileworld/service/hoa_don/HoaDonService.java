eab660d99436f3876a65c9fbc37b67e171e2a4dc
phuocdm1702 <githupstudy@gmail.com>
2025-02-27 16:47:42 +0700

    commit update DTO, Service, Controller Hoa Don

--- src/main/java/com/example/graduation_project_group_2_mobileworld/service/hoa_don/HoaDonService.java
+++ src/main/java/com/example/graduation_project_group_2_mobileworld/service/hoa_don/HoaDonService.java
@@ -1,5 +1,6 @@
 package com.example.graduation_project_group_2_mobileworld.service.hoa_don;
 
+import com.example.graduation_project_group_2_mobileworld.dto.hoa_don.HoaDonDTO;
 import com.example.graduation_project_group_2_mobileworld.entity.HoaDon;
 import com.example.graduation_project_group_2_mobileworld.repository.hoa_don.HoaDonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +10,33 @@ import java.util.List;
 
 @Service
 public class HoaDonService {
-    private HoaDonRepository hoaDonRepository;
-    @Autowired
+    private final HoaDonRepository hoaDonRepository;
+
     public HoaDonService(HoaDonRepository hoaDonRepository) {
         this.hoaDonRepository = hoaDonRepository;
     }
 
-    public List<HoaDon> getAllData(){
-        return hoaDonRepository.findAll();
+    public List<HoaDonDTO> getAllData(){
+        return hoaDonRepository.findAll()
+                .stream()
+                .map(this::toDTO)
+                .toList()
+                ;
+    }
+
+    private HoaDonDTO toDTO(HoaDon hoaDon){
+        return new HoaDonDTO(
+                hoaDon.getId(),
+                hoaDon.getIdKhachHang(),
+                hoaDon.getIdNhanVien(),
+                hoaDon.getIdPhieuGiamGia(),
+                hoaDon.getMa(),
+                hoaDon.getSoDienThoaiKhachHang(),
+                hoaDon.getLoaiDon(),
+                hoaDon.getTongTien(),
+                hoaDon.getNgayTao(),
+                hoaDon.getTongTienSauGiam(),
+                hoaDon.getTrangThai()
+        );
     }
 }
