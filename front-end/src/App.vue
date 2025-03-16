9936e686cb8533e96a8026de1b3427a97e05cca4
phuocdm1702 <githupstudy@gmail.com>
2025-03-16 14:53:15 +0700

    Commit update Footer

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,19 +1,38 @@
+<!-- Template chính -->
 <template>
-  <component :is="layout">
-    <router-view />
-    <footer-page/>
-  </component>
+  <div class="app-container">
+    <component :is="layout">
+      <router-view />
+    </component>
+    <footer-page />
+  </div>
 </template>
+
 <script lang="ts" setup>
 import { computed } from 'vue'
 import { useRouter } from 'vue-router'
 import FooterPage from "@/components/FooterPage.vue";
 
 const defaultLayout = 'default'
-
 const { currentRoute } = useRouter()
 
 const layout = computed(
   () => `${currentRoute.value.meta.layout || defaultLayout}-layout`
 )
 </script>
+
+<style scoped>
+.app-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh; /* Chiều cao tối thiểu bằng toàn bộ viewport */
+}
+
+router-view {
+  flex: 1 0 auto; /* Nội dung chính sẽ chiếm không gian còn lại */
+}
+
+footer-page {
+  flex-shrink: 0; /* Footer không bị co lại */
+}
+</style>
