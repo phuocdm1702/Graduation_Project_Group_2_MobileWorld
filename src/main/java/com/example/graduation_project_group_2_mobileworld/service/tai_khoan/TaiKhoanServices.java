b21576f2fdacf340655190b630bdaf6912a800d6
lqphuc2872006 <lequangphuc2872006@gmail.com>
2025-03-29 18:18:50 +0700

    BE + FE DangKy

--- src/main/java/com/example/graduation_project_group_2_mobileworld/service/tai_khoan/TaiKhoanServices.java
+++ src/main/java/com/example/graduation_project_group_2_mobileworld/service/tai_khoan/TaiKhoanServices.java
@@ -1,5 +1,7 @@
 package com.example.graduation_project_group_2_mobileworld.service.tai_khoan;
 
+import com.example.graduation_project_group_2_mobileworld.dto.tai_khoan.TaiKhoanDTO;
+import com.example.graduation_project_group_2_mobileworld.entity.QuyenHan;
 import com.example.graduation_project_group_2_mobileworld.entity.TaiKhoan;
 import com.example.graduation_project_group_2_mobileworld.repository.tai_khoan.TaiKhoanRepository;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,16 @@ public class TaiKhoanServices {
 
     public TaiKhoanServices(TaiKhoanRepository taiKhoanRepository) {
         this.taiKhoanRepository = taiKhoanRepository;
+    }
+
+    public String MaTaiKhoan() {
+        String maxMaTK = taiKhoanRepository.findMaxMaTK();
+        if (maxMaTK != null) {
+            int nextNumber = Integer.parseInt(maxMaTK.substring(2)) + 1;
+            return String.format("TK%05d", nextNumber);
+        } else {
+            return "TK00001";
+        }
     }
 
     public TaiKhoan add(TaiKhoan taiKhoan) {
@@ -30,5 +42,18 @@ public class TaiKhoanServices {
             return tk; // Trả về tài khoản nếu mật khẩu khớp
         }
         return null; // Trả về null nếu không khớp
+    }
+
+    public TaiKhoan addTK(TaiKhoanDTO taiKhoanDTO) {
+        QuyenHan quyenHan = new QuyenHan();
+        quyenHan.setId(2);
+        //taikhoan
+        TaiKhoan taiKhoan1 = new TaiKhoan();
+        taiKhoan1.setIdQuyenHan(quyenHan);
+        taiKhoan1.setMa(MaTaiKhoan());
+        taiKhoan1.setTenDangNhap(taiKhoanDTO.getTenDangNhap());
+        taiKhoan1.setMatKhau(taiKhoanDTO.getMatKhau());
+        taiKhoan1.setDeleted(false);
+        return taiKhoanRepository.save(taiKhoan1);
     }
 }
