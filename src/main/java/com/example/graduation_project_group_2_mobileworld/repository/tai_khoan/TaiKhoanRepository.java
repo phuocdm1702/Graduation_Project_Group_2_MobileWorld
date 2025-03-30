7e270ec90dcda6e5534bec0bff9c22b300a6850a
lqphuc2872006 <lequangphuc2872006@gmail.com>
2025-03-30 23:04:00 +0700

    BE + FE Sign up

--- src/main/java/com/example/graduation_project_group_2_mobileworld/repository/tai_khoan/TaiKhoanRepository.java
+++ src/main/java/com/example/graduation_project_group_2_mobileworld/repository/tai_khoan/TaiKhoanRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface TaiKhoanRepository extends JpaRepository<TaiKhoan, Integer> {
     @Query("SELECT MAX(t.ma) FROM TaiKhoan t WHERE t.ma LIKE 'TK%'")
     String findMaxMaTK();
@@ -16,5 +18,10 @@ public interface TaiKhoanRepository extends JpaRepository<TaiKhoan, Integer> {
 
     @Query("SELECT tk FROM TaiKhoan tk WHERE tk.tenDangNhap = :tenDangNhap AND tk.matKhau = :matKhau")
     TaiKhoan findByTenDangNhapAndMatKhau(@Param("tenDangNhap") String tenDangNhap, @Param("matKhau") String matKhau);
+
+    //checkEmail
+    Optional<TaiKhoan> findByEmail(String email);
+    Optional<TaiKhoan> findBytenDangNhap(String tenDangNhap);
+
 
 }
