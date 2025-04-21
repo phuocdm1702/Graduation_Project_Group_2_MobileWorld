eecdd04db60dcc6f04c9115ea5c565912f81ee40
phuocdm1702 <githupstudy@gmail.com>
2025-04-21 10:28:35 +0700

    Commit update upload cloud img

--- src/main/java/com/example/graduation_project_group_2_mobileworld/repository/san_pham/AnhSanPhamRepository.java
+++ src/main/java/com/example/graduation_project_group_2_mobileworld/repository/san_pham/AnhSanPhamRepository.java
@@ -6,9 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AnhSanPhamRepository extends JpaRepository<AnhSanPham, Integer> {
     @Query("SELECT a FROM AnhSanPham a ORDER BY a.id DESC")
     List<AnhSanPham> findTopNByOrderByIdDesc(int limit);
+
+    Optional<AnhSanPham> findByHashAndProductGroupKey(String hash, String productGroupKey);
+
+    Optional<AnhSanPham> findByDuongDan(String duongDan); // Add this to check by URL
 }
